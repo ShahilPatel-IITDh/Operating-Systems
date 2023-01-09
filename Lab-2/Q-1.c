ceb1fd203fdc39712dcd9583730c79c24b9fe103
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-01-09 15:47:14 +0530

    Lab-2 Q-1 (Hello World)

---
+++ Lab-2/Q-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>  
+
+int main(){
+    char str[] = "Hello World";
+    int pid = 0;
+
+    for(int i=0; i<11; i++){
+
+        // Print for the child process only.
+        if(pid == 0){
+            printf("%c (pid = %d)\n", str[i],(int) getpid());
+            // Select an random integer between 1 to 4
+            int n = (rand()%4) + 1;
+            sleep(n);
+
+            if(i != 10){
+            	pid = fork();
+            }
+        }
+    }
+    wait(NULL);
+    return 0;
+}
