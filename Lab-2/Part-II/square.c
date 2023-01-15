88442774365b23784fdc4befbb2be44f78bef418
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-01-15 19:35:58 +0530

    Part-2 files

---
+++ Lab-2/Part-II/square.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <math.h>
+
+char* itoa(int val, int base);
+
+int main(int argc, char* argv[]) {
+    if (argc == 1){
+        exit(0);
+    }
+        
+    int argument = argc-1;
+    int n = atoi(argv[argument]);
+    int ans = pow(n, 2);
+
+    int process_id = getpid();
+    printf("Square: Current process id: %u, Current result: %d\n", process_id, ans);
+
+    char* v[argc];
+    for (int i = 1; i < (argc-1) ; i++) {
+        v[i-1] = argv[i];
+    }
+        
+    v[argc-2] = itoa(ans, 10);
+    v[argument] = NULL;
+    
+    execvp(v[0], v);
+}
+
+char* itoa(int val, int base){
+    
+    static char buf[32] = {0};    
+    int i = 30;    
+    for(; val && i ; --i, val /= base)    
+        buf[i] = "0123456789abcdef"[val % base];   
+    return &buf[i+1];
+    
+}   
