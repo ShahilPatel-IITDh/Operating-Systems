2eb1e49faf31c5299e4acc0f4a74632dd75d6553
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-01-16 15:00:04 +0530

    Code commented for understanding.

--- Lab-2/Part-II/square.c
+++ Lab-2/Part-II/square.c
@@ -11,20 +11,29 @@ int main(int argc, char *argv[]){
     }
 
     else if (argc == 2){
+        // atoi: converts a character string to an integer value
         int value = atoi(argv[1]);
         int ans = value * value;
+
+        // getpid(): returns the process ID of the parent of the calling process.
         int process_id = getpid();
         printf("Square: Current process id(cpid): %d, Current result: %d\n", process_id, ans);
         return 0;
     }
 
     if (argc > 2){
+        // atoi: converts a character string to an integer value
         int value = atoi(argv[argc - 1]);
         int ans = value * value;
+
+        // getpid(): returns the process ID of the parent of the calling process.
         int process_id = getpid();
         printf("Square: Current process id (cpid): %d, Current result: %d\n", process_id, ans);
+
+        // assign argv[argc-1] the value of ans, as it is a string we use sprintf; 
         sprintf(argv[argc - 1], "%d", ans);
 
+        // use execvp to pass the next .out file and rest of the arguments in the next file.
         execvp(argv[1], argv + 1);
         return 0;
     }
