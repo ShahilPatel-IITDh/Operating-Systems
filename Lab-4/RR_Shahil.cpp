d49f61152ec484af07c962cf462ba662a3d5a6a7
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-02-02 09:51:16 +0530

    Updated code for RR.

--- Lab-4/RR_Shahil.cpp
+++ Lab-4/RR_Shahil.cpp
@@ -8,365 +8,193 @@
 
 using namespace std;
 
-vector<queue<int>> process;
-queue<pair<int, int>> ioq, pq;
-vector<int> iocheck, ptot, tr_time, rtime_total, wait_time, btime_total, ptime_total;
+// Global Variables declarations
+
+vector<queue<int>> Process;
+queue<pair<int, int>> IO_Queue, CPU_Queue;
+vector<int> iocheck, ptot, turnaroundTime, rtime_total, wait_time, burstTime, ptime_total;
 vector<float> penalty;
 
-
 int waitprocessgoingon = 0, IO_Wait = 0, ongoingpid = -1, ioongoingpid = -1;
-int timeslice = 3;
-int t_e = 0;
-unordered_map<int, vector<int>> arrival;
-bool processgoingon = false, ioprocessgoingon = false;
-    
 
+// If the timeslice is not specified through the input file, then 3 will be taken as default value .
+
+int timeslice = 3;
+int time1 = 0;
+
+unordered_map<int, vector<int>> processArrival;
+bool process_On = false, IO_Process_On = false;
+    
 void readFile(string fileLine){
     string inputText;
-    int x = 0;
-    bool flags = false;
-    bool checkFull = false;
-
     ifstream InputFile(fileLine);
-    int j = 0, sum=0;
-
-    // DC
-    x = 0;
-    flags = false;
-    checkFull = false;
-
+    int j=0, sum=0;
 
     while (getline(InputFile, inputText)){
         // Ignore the lines having <
         if (inputText[0] == '<'){
             continue;
         }
-        x = 0;
-        flags = false;
-        checkFull = false;
 
         string num;
         queue<int> q;
+
         stringstream ss(inputText);
+
         int i = 0;
         while (getline(ss, num, ' ')){
             stringstream ssnm(num);
             int x = 0;
             ssnm >> x;
-            
-            // DC
-            x = 0;
-            flags = false;
-            checkFull = false;
 
             if (i == 0){
-                tr_time.push_back(0);
+                turnaroundTime.push_back(0);
                 wait_time.push_back(0);
                 rtime_total.push_back(-1);
-                btime_total.push_back(0);
+                burstTime.push_back(0);
                 penalty.push_back(0);
                 ptot.push_back(x);
 
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-                arrival[x].push_back(j);
+                processArrival[x].push_back(j);
                 ptime_total.push_back(0);
                 iocheck.push_back(0);
             }
+
             else{
                 if (x != -1 && x != 0){
                     if (i % 2 == 1){
                         sum += x;
-                        btime_total[j] += x;
+                        burstTime[j] += x;
                     }
-                    // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
                     ptime_total[j] += x;
-                    cout << x << ",";
                     q.push(x);
                 }
             }
             i++;
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+
         }
         j++;
-
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-        process.push_back(q);
+        Process.push_back(q);
     }
-    cout << process.size() << " Size of Process= " << sum << endl;
+    cout << "Number of processes: " << Process.size() << " Size of Process = " << sum << endl;
     InputFile.close();
 
-    // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-}
-
-void startEvent(string fl){
+// void Execution(string fl){
     int time = 0;
     int tbreak = 0;
     int x = 0;
     bool flags = false;
     bool checkFull = false;
     
-    
     for (;; time++, tbreak++){
 
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-        if (arrival[time].size() > 0){
-            for (int prc = 0; prc < arrival[time].size(); prc++){
-                pq.push(make_pair(process[arrival[time][prc]].front(), arrival[time][prc]));
-                process[arrival[time][prc]].pop();
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+        if (processArrival[time].size() > 0){
+            for (int prc = 0; prc < processArrival[time].size(); prc++){
+                CPU_Queue.push(make_pair(Process[processArrival[time][prc]].front(), processArrival[time][prc]));
+                Process[processArrival[time][prc]].pop();
             }
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
         }
-        if (!processgoingon && pq.size() > 0){
-            waitprocessgoingon = pq.front().first;
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+        if (!process_On && CPU_Queue.size() > 0){
+            waitprocessgoingon = CPU_Queue.front().first;
 
-            ongoingpid = pq.front().second;
-
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+            ongoingpid = CPU_Queue.front().second;
             if (rtime_total[ongoingpid] == -1)
                 if (time - ptot[ongoingpid] - 1 < 0){
-                    // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
                     rtime_total[ongoingpid] = 0;
                 }
                 else{
                     rtime_total[ongoingpid] = time - ptot[ongoingpid] - 1;
-                    // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
                 }
-            pq.pop();
-            processgoingon = true;
+            CPU_Queue.pop();
+            process_On = true;
         }
-        if (!ioprocessgoingon && ioq.size() > 0){
-            IO_Wait = ioq.front().first;
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
 
-            ioongoingpid = ioq.front().second;
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-            ioq.pop();
+        if (!IO_Process_On && IO_Queue.size() > 0){
+            IO_Wait = IO_Queue.front().first;
+            ioongoingpid = IO_Queue.front().second;
+            IO_Queue.pop();
 
-            if (process[ioongoingpid].size() > 0){
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+            if (Process[ioongoingpid].size() > 0){
 
                 iocheck[ioongoingpid] = 0;
-                if (process[ioongoingpid].front() != 0){
-                    pq.push(make_pair(process[ioongoingpid].front(), ioongoingpid));
+                if (Process[ioongoingpid].front() != 0){
+                    CPU_Queue.push(make_pair(Process[ioongoingpid].front(), ioongoingpid));
                 }          
 
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-                pq.push(make_pair(process[ioongoingpid].front(), ioongoingpid));
-                process[ioongoingpid].pop();
+                CPU_Queue.push(make_pair(Process[ioongoingpid].front(), ioongoingpid));
+                Process[ioongoingpid].pop();
             }
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
 
-            ioprocessgoingon = true;
+            IO_Process_On = true;
         }
         if (waitprocessgoingon >= 1){
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
             waitprocessgoingon--;
         }
 
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
         if (IO_Wait >= 1){
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
             IO_Wait--;
         }
 
-        if (ioprocessgoingon && IO_Wait == 0){
-            if (process[ioongoingpid].size() > 0){
+        if (IO_Process_On && IO_Wait == 0){
+            if (Process[ioongoingpid].size() > 0){
                 continue;
             }
             else{
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-                tr_time[ioongoingpid] = time - ptot[ioongoingpid] + 1;
-                wait_time[ioongoingpid] = tr_time[ioongoingpid] - btime_total[ioongoingpid];
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-                penalty[ioongoingpid] = (btime_total[ioongoingpid] + wait_time[ioongoingpid]) * 1.0 / btime_total[ioongoingpid];
+                turnaroundTime[ioongoingpid] = time - ptot[ioongoingpid] + 1;
+                wait_time[ioongoingpid] = turnaroundTime[ioongoingpid] - burstTime[ioongoingpid];
+                penalty[ioongoingpid] = (burstTime[ioongoingpid] + wait_time[ioongoingpid]) * 1.0 / burstTime[ioongoingpid];
             }
-            ioprocessgoingon = false;
+            IO_Process_On = false;
         }
 
-        if (processgoingon && waitprocessgoingon == 0)
+        if (process_On && waitprocessgoingon == 0)
         {
-            // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
 
-            if (process[ongoingpid].size() > 0)
+            if (Process[ongoingpid].size() > 0)
             {
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
                 if (iocheck[ongoingpid] == 0)
                 {
                     iocheck[ongoingpid] = 1;
-                    ioq.push(make_pair(process[ongoingpid].front(), ongoingpid));
-                    // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-                    process[ongoingpid].pop();
+                    IO_Queue.push(make_pair(Process[ongoingpid].front(), ongoingpid));
+                    Process[ongoingpid].pop();
                 }
             }
             else{
-                tr_time[ongoingpid] = time - ptot[ongoingpid] + 1;
-                wait_time[ongoingpid] = tr_time[ongoingpid] - ptime_total[ongoingpid];
-
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-                penalty[ongoingpid] = (btime_total[ongoingpid] + wait_time[ongoingpid]) * 1.0 / btime_total[ongoingpid];
+                turnaroundTime[ongoingpid] = time - ptot[ongoingpid] + 1;
+                wait_time[ongoingpid] = turnaroundTime[ongoingpid] - ptime_total[ongoingpid];
+                penalty[ongoingpid] = (burstTime[ongoingpid] + wait_time[ongoingpid]) * 1.0 / burstTime[ongoingpid];
             }
-            // cout << "{" << ongoingpid << "}" << endl;
             tbreak = 0;
-            processgoingon = false;
-            if (pq.empty() && ioq.empty()){
+            process_On = false;
+
+            if (CPU_Queue.empty() && IO_Queue.empty()){
                 break;
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
             }
         }
-        if (tbreak == timeslice){
-            if (processgoingon)
-            {
-                pq.push(make_pair(waitprocessgoingon, ongoingpid));
-                processgoingon = false;
-                // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-                tbreak = 0;
-            }
+        if (tbreak == timeslice && process_On==true){
+            CPU_Queue.push(make_pair(waitprocessgoingon, ongoingpid));
+            process_On = false;
+            tbreak = 0;
         }
     }
-    t_e = time;
-    ofstream MyWriteFile(fl + "_" + to_string(timeslice) + "_rob.csv");
-    float rt = 0, bst = 0, tat = 0, wt = 0, pta = 0, sz = rtime_total.size();
-    cout << "_________________________Individual Process____________________________" << endl;
-    MyWriteFile << "Process_ID,Response_Time,Wait_Time,Burst_Time,Turn_Around_Time,Penalty_Ratio\n";
-    for (int i = 0; i < sz; i++){
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+    time1 = time;
 
-        MyWriteFile << i << "," << rtime_total[i] << "," << wait_time[i] << "," << btime_total[i] << "," << tr_time[i] << "," << penalty[i] << "\n";
-        cout << "Process ID=" << i << " Response Time = " << rtime_total[i] << " Wait Time = " << wait_time[i] << " Burst Time = " << btime_total[i] << " Turn Around Time = " << tr_time[i] << " Penalty Ratio = " << penalty[i] << endl;
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+    float runTime = 0, avgBurstTime = 0, avgTurnaroundTime = 0, avgWaitTime = 0, penaltyRatio = 0, sizeOfProcess = rtime_total.size();
 
-        rt += rtime_total[i];
-        bst += btime_total[i];
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+    cout << "Process_ID  --  Response_Time  --  Wait_Time  --  Burst_Time  --  Turn_Around_Time  --  Penalty_Ratio"<<endl;
 
-        tat += tr_time[i];
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
+    for (int i = 0; i < sizeOfProcess; i++){        
+        cout << "Process ID = " << i << " Response Time = " << rtime_total[i] << " Wait Time = " << wait_time[i] << " Burst Time = " << burstTime[i] << " Turn Around Time = " << turnaroundTime[i] << " Penalty Ratio = " << penalty[i] << endl;
 
-        wt += wait_time[i];
-        // DC
-                x = 0;
-                flags = false;
-                checkFull = false;
-
-        pta += penalty[i];
+        penaltyRatio += penalty[i];
+        avgWaitTime += wait_time[i];
+        avgTurnaroundTime += turnaroundTime[i];
+        avgBurstTime += burstTime[i];
+        runTime += rtime_total[i];
     }
-    cout << "_________________________System Average____________________________"
-         << "\n";
-    MyWriteFile << sz * 1.0 / (t_e - 1) << " " << rt / sz << "," << wt / sz << "," << bst / sz << "," << tat / sz << "," << pta / sz << endl;
-    cout << "Response Time = " << rt / sz << " Wait Time = " << wt / sz << " Burst Time = " << bst / sz << " Turn Around Time = " << tat / sz << " Penalty Ratio = " << pta / sz << endl;
-    cout << "System throughput = " << sz * 1.0 / (t_e - 1) << endl;
-    MyWriteFile.close();
+    cout << "Response Time = " << runTime / sizeOfProcess << " Wait Time = " << avgWaitTime / sizeOfProcess << " Burst Time = " << avgBurstTime / sizeOfProcess << " Turn Around Time = " << avgTurnaroundTime / sizeOfProcess << " Penalty Ratio = " << penaltyRatio / sizeOfProcess << endl;
+
+    cout << "System throughput = " << sizeOfProcess * 1.0 / (time1 - 1) << endl;
 }
 
 int main(int argc, char *argv[]){
@@ -379,14 +207,17 @@ int main(int argc, char *argv[]){
     if(argc == 2){
         fileLine = argv[1];
     }
+
+    // Not working properly
     if(argc == 3){
         // Convert the argv[2] to int
         timeslice = atoi(argv[2]);
     }
+
     else{
-        timeslice = 5;
+        timeslice = 3;
     } 
     readFile(fileLine);
-    startEvent(fileLine);
+    // Execution(fileLine);
     return 0;
 }
