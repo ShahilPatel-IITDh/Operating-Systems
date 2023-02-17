f91ba14ce41b80bcea960f3279820f7f4741e496
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-02-17 23:31:56 +0530

    Code not compiling properly

--- Lab-6/Part2-1-a.cpp
+++ Lab-6/Part2-1-a.cpp
@@ -37,18 +37,54 @@ class Pixel{
         }
 };
 
-atomic_flag lock = ATOMIC_FLAG_INIT;
+atomic_flag threadLock = ATOMIC_FLAG_INIT;
 
 int getNewColour(int colour, int blur){
     int newColour = colour * (0.5/blur);
     return newColour;
 }
 
+// Function to converts an image to Grayscale by iterating over pixels and then rows and updating pixel values
+void RGBtoGrayScale(int width, int height, vector<vector<Pixel>> &matrix){
+
+    for (int i = 0; i < height; i++){        
+        for (int j = 0; j < width; j++){
+
+            // Lock for the current row, so that no other thread can access it
+            // We use a while loop because the atomic_flag_test_and_set function returns true if the lock is already set. This is a spin lock
+            while (atomic_flag_test_and_set(&threadLock));
+            
+            int colourRed = matrix[i][i].getRed();
+            int colourGreen = matrix[i][j].getGreen();
+            int colourBlue = matrix[i][j].getBlue();
+
+            // weighted average of red, green and blue is calculated and then assigned to all three values
+            // this is done to convert the image to grayscale
+            // weighted average = (0.299 * red) + (0.587 * green) + (0.114 * blue)
+            int newRed = (colourBlue * 0.114) + (colourRed * 0.299) + (colourGreen * 0.587);
+            int newGreen = (colourBlue * 0.114) + (colourRed * 0.299) + (colourGreen * 0.587);
+            int newBlue = (colourBlue * 0.114) + (colourRed * 0.299) + (colourGreen * 0.587);
+
+            matrix[i][j].setRed(newRed);
+            matrix[i][j].setGreen(newGreen);
+            matrix[i][j].setBlue(newBlue);
+
+            // Unlock the current row
+            atomic_flag_clear(&threadLock);
+        }
+    }
+}
+
 void HorizontalBlur(int width, int height, vector<vector<Pixel>> &matrix){
     
     int blurAmount = 30;
+
     for (int i = 0; i < height; i++){
         for (int j = 0; j < width; j++){
+            
+            // Lock the thread for the current pixel, so that no other thread can access it
+            // We use a while loop because the atomic_flag_test_and_set function returns true if the lock is already set. This is a spin lock          
+            while (atomic_flag_test_and_set(&threadLock));
 
             int colourRed = matrix[i][j].getRed()/2;
             int colourGreen = matrix[i][j].getGreen()/2;
@@ -70,6 +106,9 @@ void HorizontalBlur(int width, int height, vector<vector<Pixel>> &matrix){
                 matrix[i][j].setRed(colourRed);
                 matrix[i][j].setBlue(colourBlue);
                 matrix[i][j].setGreen(colourGreen);
+
+                // Unlock the thread for the current pixel
+                // atomic_flag_clear(&threadLock);
                 continue;
             }
 
@@ -85,31 +124,9 @@ void HorizontalBlur(int width, int height, vector<vector<Pixel>> &matrix){
             matrix[i][j].setRed(colourRed);
             matrix[i][j].setBlue(colourBlue);
             matrix[i][j].setGreen(colourGreen);
-        }
-    }
-}
-
-// Function to converts an image to Grayscale by iterating over pixels and then rows and updating pixel values
-void RGBtoGrayScale(int width, int height, vector<vector<Pixel>> &matrix){
-
-    for (int i = 0; i < height; i++){
-        while(atomic_flag_test_and_set(&lock));
-        
-        for (int j = 0; j < width; j++){
-            int colourRed = matrix[i][i].getRed();
-            int colourGreen = matrix[i][j].getGreen();
-            int colourBlue = matrix[i][j].getBlue();
-
-            // weighted average of red, green and blue is calculated and then assigned to all three values
-            // this is done to convert the image to grayscale
-            // weighted average = (0.299 * red) + (0.587 * green) + (0.114 * blue)
-            int newRed = (colourBlue * 0.114) + (colourRed * 0.299) + (colourGreen * 0.587);
-            int newGreen = (colourBlue * 0.114) + (colourRed * 0.299) + (colourGreen * 0.587);
-            int newBlue = (colourBlue * 0.114) + (colourRed * 0.299) + (colourGreen * 0.587);
-
-            matrix[i][j].setRed(newRed);
-            matrix[i][j].setGreen(newGreen);
-            matrix[i][j].setBlue(newBlue);
+            
+            // Unlock the thread for the current pixel
+            atomic_flag_clear(&threadLock);
         }
     }
 }
@@ -158,10 +175,18 @@ int main(int argc, char* argv[]){
 
     auto beginStamp = chrono::high_resolution_clock::now(); // Starting the clock
 
+    thread thread1(RGBtoGrayScale, width, height, ref(matrix));
+    thread thread2(HorizontalBlur, width, height, ref(matrix));
+
+    // Wait for the threads to finish
+    thread1.join();
+    thread2.join();
+
     // call the function to convert the image to grayscale (T1 function)
-    RGBtoGrayScale(width, height, matrix);
+    // RGBtoGrayScale(width, height, matrix);
+    
     // call the function to apply the horizontal blur (T2 function)
-    HorizontalBlur(width, height, matrix);
+    // HorizontalBlur(width, height, matrix);
 
     auto endStamp = chrono::high_resolution_clock::now(); //Stopping the clock
     auto duration = chrono::duration_cast<chrono::microseconds>(endStamp - beginStamp);//Calculating the time taken by T1 and T2
