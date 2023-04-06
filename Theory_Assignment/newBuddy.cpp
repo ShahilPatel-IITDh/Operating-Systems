e25e5e96f660c5e5b92bf05d354ddebee41eff90
Shahil Patel <83348636+ShahilPatel-IITDh@users.noreply.github.com>
2023-04-06 22:45:27 +0530

    Giving incoorect  output for the size of free blocks

--- Theory_Assignment/newBuddy.cpp
+++ Theory_Assignment/newBuddy.cpp
@@ -1,120 +1,169 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
 #include <map>
 #include <string>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <map>
+
+#define ll long long int
+#define FOR(a, b, c) for (int a = b; a < c; a++)
 
 using namespace std;
+class Block{
+    public:
+        bool occupied;
+        int size;
+        char processName;
+        int processSize;    
 
-struct Block{
-    bool occupied;
-    int size;
-    char process;
+    Block(int size, bool occupied, char process, int processSize) {
+        this->size = size;
+        this->occupied = occupied;
+        this->processName = process;
+        this->processSize = processSize;
+    }
 };
 
-Block generateBlock(int size, bool occupied, char process) {
-    Block block ;
-    block.size = size;
-    block.occupied = occupied;
-    block.process = process;
-    return block;
-}
 
-void splitBlock(vector<Block> &memory, int index, int target_size) {
-    while (memory[index].size > target_size) {
-        memory.insert(memory.begin() + index + 1, generateBlock(memory[index].size / 2, false, '-'));
-        memory[index].size /= 2;
+void splitBlock(vector<Block> &memory, int index, int requiredSize, long long int lowerLimit) {
+    if (memory[index].size >= requiredSize*2 && !memory[index].occupied) {
+        // the current block is free and larger than double the requested size, so split it into two
+        int newSize = memory[index].size / 2;
+        memory.insert(memory.begin() + index + 1, Block(newSize, false, '-', 0));
+        memory[index].size = newSize;
+    } 
+    else {
+        // the current block is either occupied or smaller than double the requested size, so don't split it
+        return;
     }
+    // check if the new block should be split further
+    splitBlock(memory, index + 1, requiredSize, lowerLimit);
 }
 
-void merge_blocks(vector<Block> &memory) {
-    for (int i = 0; i < memory.size() - 1;) {
+void mergeBlocks(vector<Block> &memory){
+    // merge the free blocks
+    for (int i = 0; i < memory.size()-1;) {
         if (!memory[i].occupied && !memory[i + 1].occupied && memory[i].size == memory[i + 1].size){
             memory[i].size *= 2;
             memory.erase(memory.begin() + i + 1);
         }
-        else {
+        else{
             i++;
         }
     }
 }
 
-void processRequest(vector<Block> &memory, char process, int size) {
-    if (size == 0) {
+int nearestPowerOf2(int n){
+    int x = (int) ceil(log2(n));
+    return (int)pow(2, x);
+}
+
+int findLowestBlock(int index, vector<Block> &memory, int givenSize, long long int lowerLimit){
+    for(int i=0; i<memory.size(); i++){
+        if(memory[i].occupied == false && memory[i].size == givenSize){
+            index = i;
+            break;
+        }
+    }
+}
+
+void processRequest(vector<Block> &memory, char processName, int givenSize, long long int lowerLimit){
+    // if the size is zero, then we need to free the block
+    if(givenSize == 0){
         for (Block &block : memory) {
-            if (block.process == process) {
+            if (block.processName == processName) {
+                // free the block
                 block.occupied = false;
-                block.process = '-';
+                // set the process name to '-'
+                block.processName = '-';
                 break;
             }
         }
-        merge_blocks(memory);
+        // merge the free blocks
+        mergeBlocks(memory);
     }
     else {
         int index = -1;
-        for (int i = memory.size() - 1; i >= 0; i--) {
-            if (!memory[i].occupied && memory[i].size >= size) {
+        for (int i = 0; i < memory.size(); i++) {
+            // if the size of the block is greater than or equal to the given size and the block is free, then we can allocate the block
+            if (memory[i].occupied==false && memory[i].size == nearestPowerOf2(givenSize)) {
+                // set the index of the block
                 index = i;
+                break;
             }
         }
-        if (index != -1) {
-            splitBlock(memory, index, size);
-            memory[index].occupied = true;
-            memory[index].process = process;
-        }
-    }
-}
-
-void printState(const vector<Block> &memory) {
-    for (const Block &block : memory) {
-        if (block.occupied) {
-            cout << block.process << " : " << block.size << endl;
+        if (index == -1){
+            findLowestBlock(memory, givenSize, lowerLimit)
+            return;
         }
         else {
-            cout << "Free Block : " << block.size << endl;
+            splitBlock(memory, index, givenSize, lowerLimit);
+            memory[index].occupied = true;
+            memory[index].processName = processName;
+            memory[index].processSize = givenSize;
         }
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if(argc != 2){
+        cout << "Usage: " << argv[0] << " <input file>" << endl;
+        return 1;
+    }
     int testCases;
-    cin >> testCases;
+    fstream inputFile;
+    inputFile.open(argv[1], ios::in);
+
+    if(!inputFile.is_open()){
+        cout << "Error opening file " << argv[1] << endl;
+        return 1;
+    }
+    // read testcase from the first line of the inputFile
+    inputFile >> testCases;
     while (testCases--){
-        int U, L;
-        cin >> U >> L;
-        vector<Block> memory = {generateBlock(pow(2, U), false, '-')};
-
+        int U;
+        int L;
+        // read the U, L from the inputFile, there is gap of 1 line between the testcase and U, L value
+        inputFile >> U >> L;
+        
+        long long int upperLimit = pow(2, U);
+        long long int lowerLimit = pow(2, L);
         string requestLine;
-        getline(cin, requestLine); // consume the newline after U and L
+        int processSize = 0;
+        // convert the block return to vector
+        vector<Block> memory = {Block(upperLimit, false, '-', processSize)};
 
-        map<char, int> processSizes;
-        while (getline(cin, requestLine) && !requestLine.empty()) {
-            char process = requestLine[0];
-            int size = stoi(requestLine.substr(2));
-            if (size == 0) {
-                processSizes.erase(process);
-            }
-            else {
-                processSizes[process] = size;
-            }
 
-            int target_size = 0;
-            for (const auto &p : processSizes) {
-                target_size = max(target_size, p.second);
-            }
+        while (inputFile>>requestLine) {
+            string processName = requestLine;
+            // convert the string to char using c_str()
+            char process = processName.c_str()[0];
+            inputFile >> requestLine;
+            string sizeString = requestLine;
+            int size = stoll(sizeString);
+            processRequest(memory, process, size, lowerLimit);
+        }
 
-            bool allocated = false;
-            for (int i = memory.size() - 1; i >= 0; i--) {
-                if (!memory[i].occupied && memory[i].size >= target_size) {
-                    processRequest(memory, process, target_size);
-                    allocated = true;
-                    break;
-                }
+        for (const Block &block : memory) {
+            if(block.size < lowerLimit){
+                int x = -1;
+                x++;
+            }
+            if(block.occupied == false){
+                cout<<"Free Block: "<<block.size<<endl;
             }
 
-            if (!allocated) {
-                cout << "Unable to allocate memory for process " << process << endl;
+            else if (block.occupied == true) {
+                cout << block.processName << ": " << block.processSize << endl;
             }
+        }
+        if(testCases) {
+            cout << endl;
         }
     }
     return 0;
